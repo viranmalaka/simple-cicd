a4596f590976794c9c33fcdc19ea83961ea03913
vmal3951 <viran.malaka@syscolabs.com>
2021-04-10 19:33:45 +0530

    Change the title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          I created a CICD with github
         </h1>
 
         <p className={styles.description}>
